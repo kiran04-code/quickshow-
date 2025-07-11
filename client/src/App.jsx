adb91152ea5a0306c949660efaf5d420ce254b39
kiran04-code <kr551344@gmail.com>
2025-07-12 01:15:12 +0530

    init-projext

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className='bg-red-500'>we</h1>
+    </div>
+  )
+}
+
+export default App
