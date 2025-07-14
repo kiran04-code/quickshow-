7457197597412c78651cab6845748de4965532b6
kiran04-code <kr551344@gmail.com>
2025-07-14 12:51:07 +0530

    new

--- client/src/pages/Admin/ListShow.jsx
+++ client/src/pages/Admin/ListShow.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Text from './components/text'
-
+import Loading from '../../components/Loading'
 const ListShow = () => {
-  return (
+  const [show,setShow] = useState([])
+  const [loading,setLoading] = useState(true)
+  return loading ? (
     <div>
          <div className=''>
       <Text text2="ListShow"/>
-    <h1> ListBooking </h1>
+    <h1> ListShow </h1>
     </div>
     </div>
-  )
+  ):<Loading/>
 }
 
 export default ListShow
